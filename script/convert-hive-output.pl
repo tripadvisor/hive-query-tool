c53152bac6f0634f80a89479ead0076c42116fc4
Stephen R. Scaffidi <stephen@scaffidi.net>
2013-06-24 12:21:10 -0400

    more license headers

--- script/convert-hive-output.pl
+++ script/convert-hive-output.pl
@@ -1,4 +1,21 @@
 #!/usr/bin/env perl
+##############################################################################
+#                                                                            #
+#   Copyright 2013 TripAdvisor, LLC                                          #
+#                                                                            #
+#   Licensed under the Apache License, Version 2.0 (the "License");          #
+#   you may not use this file except in compliance with the License.         #
+#   You may obtain a copy of the License at                                  #
+#                                                                            #
+#       http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                            #
+#   Unless required by applicable law or agreed to in writing, software      #
+#   distributed under the License is distributed on an "AS IS" BASIS,        #
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+#   See the License for the specific language governing permissions and      #
+#   limitations under the License.                                           #
+#                                                                            #
+##############################################################################
 #
 # This is a simple script that converts the output from a hive job that was written
 # to a local directory into a gzipped CSV file.
@@ -7,22 +24,19 @@
 #   ./convert-hive-output.pl /path/to/hive/output /path/to/csvfile.csv.gz
 #
 # @Author: Stephen R. Scaffidi <sscaffidi@tripadvisor.com>
-# @Date: Sept. 2012
+# @Date: June 2013
 #
 use strict;
 use warnings;
 use feature ':5.10.1';
 use autodie;
 use File::Slurp qw(read_dir);
-# TODO: find something that doesn't need zlib headers and use that instead.
-#use PerlIO::gzip;
 use IO::Compress::Gzip qw($GzipError);
 use Text::CSV;
 
 my $in_dir   = shift;
 my $out_file = shift;
 
-#open my $out_fh, ">:gzip", $out_file;
 my $out_fh = IO::Compress::Gzip->new( $out_file )
   or die "Couldn't initialize FH for gzip output: [$GzipError]";
 
